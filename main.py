2845fe9cdc0b397e8b054b42898f201c9d320b7f
Vyacheslav Chernyak <Vachernyak@mts.ru>
2023-11-29 21:36:09 +0300

    Added 2 task

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
+import json
+from pprint import pprint
+
 import sqlalchemy
+from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 import psycopg2
-from models import create_tables, Publisher, Book, Stock, Sale, Shop
+from models import *
 
 DSN = "postgresql://postgres:postgres@localhost:5432/netology_db"
 engine = sqlalchemy.create_engine(DSN)
 create_tables(engine)
-
+con = engine.connect()
 Session = sessionmaker(bind=engine)
 session = Session()
+with open('./tests_data.json', 'r') as fd:
+    data = json.load(fd)
 
-# publisher1 = Publisher(name='Пушкин')
-# book1 = Book(title='Капитанская дочка', id_publisher=1)
-# shop1 = Shop(name='Буквоед')
-# stock1 = Stock(id_book=1, id_shop=1, count=2)
-# session.add_all()
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
 session.commit()
-
 session.close()
+find = input("Введите имя издателя или id: ")
+q = """select b.title ,s2."name" ,s3.price ,s3.date_sale 
+from publisher p 
+left join book b on p.id = b.id_publisher 
+left join stock s on b.id = s.id_book 
+left join shop s2 on s.id_shop = s.id 
+left join sale s3 on s3.id_stock = s2.id 
+where p.name = '{}' and s3.date_sale notnull; """.format(find)
+for i in con.execute(text(q)).fetchall():
+    print(f'{i[0]} | {i[1]} | {i[2]} | {i[3]}')
+
+# O’Reilly
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 import psycopg2
+
 Base = declarative_base()
 
 
@@ -8,7 +9,8 @@ class Publisher(Base):
     __tablename__ = 'publisher'
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
-
+    def __str__(self):
+        return f'name: {self.name}'
 
 class Book(Base):
     __tablename__ = 'book'
@@ -16,6 +18,8 @@ class Book(Base):
     title = sq.Column(sq.String(length=40))
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
     publisher = relationship(Publisher, backref='book')
+    def __str__(self):
+        return f'title: {self.title}, id_p: {self.id_publisher}'
 
 
 class Shop(Base):
@@ -29,20 +33,21 @@ class Stock(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
-    book = relationship(Book, backref=id)
-    shop = relationship(Shop, backref=id)
+    book = relationship(Book, backref='book')
+    shop = relationship(Shop, backref='shop')
     count = sq.Column(sq.Integer)
 
 
 class Sale(Base):
     __tablename__ = 'sale'
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=True)
+    price = sq.Column(sq.FLOAT, nullable=True)
     date_sale = sq.Column(sq.Date, nullable=True)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
-    stock = relationship(Stock, backref=id)
+    stock = relationship(Stock, backref='stock')
     count = sq.Column(sq.Integer)
 
+
 def create_tables(engine):
-    # Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
